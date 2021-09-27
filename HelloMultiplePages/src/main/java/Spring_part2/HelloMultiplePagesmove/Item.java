89ac5193fdd2051185c67a26c9a4a3f9e264e588
Santiago Delamer <80407833+Stdela@users.noreply.github.com>
2021-09-27 09:13:47 -0300

    Add files via upload

---
+++ HelloMultiplePages/src/main/java/Spring_part2/HelloMultiplePagesmove/Item.java
@@ -0,0 +1,14 @@
+
+package Spring_part2.HelloMultiplePagesmove;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+private String name;
+private String type;
+}
